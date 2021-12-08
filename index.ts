475858dd0a7810c13a08285da61c7b9bb64c47cd
Fenix4088 <fenix4088@gmail.com>
2021-12-08 19:19:55 +0200

    Started framework

--- index.ts
+++ index.ts
@@ -2,14 +2,54 @@ import http from 'http';
 
 const PORT = process.env.PORT || 5000;
 
+type IEndpoints = {
+      [k in `/${string}`]: IEndpointsContent
+}
+
+type IEndpointsContent = {
+      [k in TCrudTitles]: () => void
+}
+
+type TCrudTitles = 'GET' | 'POST' | 'DELETE';
+
+
+class Router {
+      readonly endpoints: IEndpoints
+      constructor() {
+            this.endpoints = {
+                  '/users': {
+                        'GET': () => {},
+                        'POST': () => {},
+                        'DELETE': () => {}
+                  }
+            } 
+      }
+
+      request: () => {
+
+      }
+}
+
+
 const server = http.createServer((req, res) => {
 
-      res.end('Server working');
+      res.writeHead(200, {
+            'Content-type': 'application/json'
+      })
+      
+      if(req.url === '/users') {
+            return res.end(JSON.stringify({
+                  id: 1,
+                  name: 'User'
+            }))
+      }
+
+      if(req.url === '/posts') {
+            return res.end('POSTS')
+      }
       
 });
 
 server.listen(PORT, () => {
-      debugger;
       console.log(`Server started ${PORT}`);
-      
 });
